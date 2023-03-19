dfbd75152728de2eba0191d465781be8433dc07d
cwenan <wenangongzi@gmail.com>
2023-03-19 21:47:47 +0800

    the first commit

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,26 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "浪里",
+      description: "浪里指南",
+    },
+
+    // "/": {
+    //   lang: "en-US",
+    //   title: "Docs Demo",
+    //   description: "A docs demo for vuepress-theme-hope",
+    // },
+    
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
